46ec06fe612027936629064d0b5a84fda1f98113
Yuki Kodama <endflow.net@gmail.com>
2017-03-26 19:12:13 +0900

    next

--- fukumen.ml
+++ fukumen.ml
@@ -11,15 +11,25 @@ let print list =
 
 let next list' =
   let list = List.rev list' in
-  match list with
-  | [] -> []
-  | (c, n) :: rest -> List.rev ((c, n + 1) :: rest)
+  let rec loop dn ls =
+    match ls with
+    | [] -> dn
+    | (c, n) :: rest ->
+      if n = 2
+      then loop ((c, 0) :: dn) rest (* carry over *)
+      else loop (List.append (List.append dn [(c, n + 1)]) rest) [] (* break *)
+  in List.rev (loop [] list)
 
 let () =
-  let data = [("H", 0); ("A", 0); ("C", 0); ("K", 0); ("E", 0); ("R", 0)] in
-  print data;
-  let data' = next data in
-  print data';
-  let data'' = next data' in
-  print data''
-  (* assert ((expression "1+2" 0) = (Some ["1"; "+"; "2"], 3)); *)
+  let s0 = [("A", 0); ("B", 0); ("C", 0)] in
+  print s0;
+  let s1 = next s0 in
+  print s1;
+  let s2 = next s1 in
+  print s2;
+  let s3 = next s2 in
+  print s3;
+  let s4 = next s3 in
+  print s4;
+
+  (* let data = [("H", 0); ("A", 0); ("C", 0); ("K", 0); ("E", 0); ("R", 0)] in *)
